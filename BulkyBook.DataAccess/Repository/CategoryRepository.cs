10d2480c7b22f32aec1a70e61a421043a127fe8d
New Account Name <minhazul.hasan.sohan@gmail.com>
2023-08-07 00:57:40 +0600

    Interface and Service for Category

--- BulkyBook.DataAccess/Repository/CategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,22 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository
 {
-    internal class CategoryRepository
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            this._db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
     }
 }
